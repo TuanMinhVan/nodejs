120ff801edf3d08f50031ec107bf2346abe6b600
TuanMinhVan <leetuanbmt@gmail.com>
2021-04-23 14:07:59 +0700

    change

--- index.js
+++ index.js
@@ -1,62 +1,19 @@
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const socketio = require('socket.io');
-const formatMessage = require('./utils/messages');
-const {
-  userJoin,
-  getCurrentUser,
-  userLeave,
-  getRoomUsers
-} = require('./utils/users');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketio(server);
-app.use(express.static(path.join(__dirname, 'public')));
-const botName = 'ChatCord Bot';
-io.on('connection', socket => {
-  socket.on('joinRoom', ({ username, room }) => {
-    const user = userJoin(socket.id, username, room);
-    socket.join(user.room);
-    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
-    socket.broadcast
-      .to(user.room)
-      .emit(
-        'message',
-        formatMessage(botName, `${user.username} has joined the chat`)
-      );
+var port = process.env.PORT || 3000;
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
-    io.to(user.room).emit('roomUsers', {
-      room: user.room,
-      users: getRoomUsers(user.room)
-    });
-  });
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
 
-  socket.on('chatMessage', msg => {
-    const user = getCurrentUser(socket.id);
-
-    io.to(user.room).emit('message', formatMessage(user.username, msg));
-  });
-
-  // Runs when client disconnects
-  socket.on('disconnect', () => {
-    const user = userLeave(socket.id);
-
-    if (user) {
-      io.to(user.room).emit(
-        'message',
-        formatMessage(botName, `${user.username} has left the chat`)
-      );
-
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      });
-    }
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    console.log('message: ' + msg);
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+http.listen(port, function(){
+  console.log('listening on *:3086');
+});
