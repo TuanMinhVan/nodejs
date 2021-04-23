df3d279e642db6a4796e5b09db8db414cab6cdfd
TuanMinhVan <leetuanbmt@gmail.com>
2021-04-23 08:43:11 +0700

    fix error

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = 5000;
+const app = require('express')()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http);
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+io.on('connection', socket => {
+    chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+    socket.on('disconnect', () => {
+        socket.leave(chatID)
+    })
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID':receiverChatID,
+        })
+    })
+});
+http.listen(PORT)
