92ebf94d5506195796329defaae43ac1ee31613f
TuanMinhVan <leetuanbmt@gmail.com>
2021-05-26 09:11:01 +0700

    change

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-'use strict';
+const app = require('express');
+const httpServer = require('http').createServer(app);
+const io = require('socket.io')(httpServer, {
+    cors: true,
+    origins: ["*"]
+});
 
-const express = require('express');
-const socketIO = require('socket.io');
+const { createGame } = require('./util/words');
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    socket.on('notify', (gameId) => {
+        createGame().then(words => {
+            io.emit('notify', words);
+            console.log("Someone is starting a game");
+        })
+    })
+
+    socket.on('gameUpdate', ({ gameId, words }) => {
+        io.to(gameId).emit(gameId, words);
+    })
+
+    socket.on('joinGame', ({ gameId }) => {
+        socket.join(gameId);
+        console.log("a player joined the room " + gameId);
+        socket.to(gameId).emit('joinGame', "A player joined the game!");
+    })
+});
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const io = socketIO(server);
-
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
-
-io.on('notify', (data)=> {
-  io.emit('notify', {'name':'thông báo'});
-  io.broadcast.emit('notify', {'name':'thông báo'});
-});
-
+httpServer.listen(PORT, () => console.log('Server is running on port ' + PORT));
 setInterval(() => io.emit('time', new Date().toTimeString()), 5000);
