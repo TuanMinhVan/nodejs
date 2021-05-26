42d94030a36ea10397a473fd4e9e050908e00728
TuanMinhVan <leetuanbmt@gmail.com>
2021-05-26 10:00:32 +0700

    change

--- index.js
+++ index.js
@@ -1,39 +1,31 @@
+'use strict';
+
 const express = require('express');
 const socketIO = require('socket.io');
-const httpServer = require('http').createServer(express);
+
 const PORT = process.env.PORT || 3000;
-
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));    
-
-const io = socketIO(server, {
-    cors: true,
-    origins: ["*"]
-});
 const INDEX = '/index.html';
 const { createGame } = require('./util/words');
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-io.on("connection", (socket) => {
-    console.log("a user connected");
+const io = socketIO(server);
 
-    socket.on('notify', (gameId) => {
-        createGame().then(words => {
-            io.emit('notify', words);
-            console.log("Someone is starting a game");
-        })
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('notify', (gameId) => {
+    createGame().then(words => {
+        io.emit('notify', words);
+        console.log("Someone is starting a game");
     })
-
-    socket.on('gameUpdate', ({ gameId, words }) => {
-        io.to(gameId).emit(gameId, words);
-    })
-
-    socket.on('joinGame', ({ gameId }) => {
-        socket.join(gameId);
-        console.log("a player joined the room " + gameId);
-        socket.to(gameId).emit('joinGame', "A player joined the game!");
-    })
+})
 });
 
-// httpServer.listen(PORT, () => console.log('Server is running on port ' + PORT));
+io.on('notify', function (data) {
+  io.emit('notify', data);
+  io.broadcast.emit('notify', data);
+});
+
 setInterval(() => io.emit('time', new Date().toTimeString()), 5000);
