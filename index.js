9dfa61c3863416080a2fe1e120bf7270ccae355d
TuanMinhVan <leetuanbmt@gmail.com>
2021-05-26 10:05:19 +0700

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
-const { createGame } = require('./util/words');
+// const { createGame } = require('./util/words');
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
@@ -15,17 +15,11 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
-  socket.on('notify', (gameId) => {
-    createGame().then(words => {
-        io.emit('notify', words);
-        console.log("Someone is starting a game");
-    })
-})
+  socket.on('notify', () => {
+    io.emit('notify', "Client disconnected");
+  });
 });
 
-io.on('notify', function (data) {
-  io.emit('notify', data);
-  io.broadcast.emit('notify', data);
-});
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 5000);
