2b9ab3967dd159c7034dbd41aeb480b744d45c4d
TuanMinhVan <leetuanbmt@gmail.com>
2021-05-26 10:11:22 +0700

    change

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('notify', () => socket.emit('notify',{'event':'notify'}));
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
